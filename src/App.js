f38676a6d50f8826f42aff79ec6d178a06df081d
FelipeSilva99 <felipe.rl.silva@hotmail.com>
2021-06-24 11:25:56 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    n1: "",
+    n2: "",
+    res: ""
+  }
+
+  handleChange = (e, type) => {
+    this.setState({
+      [type]: Number(e.target.value)
+    })
+  };
+
+  divider = () => {
+    this.setState({
+      res: this.state.n1 / this.state.n2
+    })
+  }
+
+  mult = () => {
+    this.setState({
+      res: this.state.n1 * this.state.n2
+    })
+  }
+
+  mais = () => {
+    this.setState({
+      res: this.state.n1 + this.state.n2
+    })
+  }
+
+  menos = () => {
+    this.setState({
+      res: this.state.n1 - this.state.n2
+    })
+  }
+
+  limpar = () => {
+    this.setState({
+      n1: "",
+      n2: "",
+      res: ""
+    })
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <h1></h1>
+          <input onChange={(e) => this.handleChange(e, 'n1')} placeholder='Preencha com numero' />
+          <input onChange={(e) => this.handleChange(e, 'n2')} placeholder='Preencha com numero' />
+          <button onClick={this.divider}>/</button>
+          <button onClick={this.mult}>*</button>
+          <button onClick={this.menos}>-</button>
+          <button onClick={this.mais}>+</button>
+          <button onClick={this.limpar}>Limpar</button>
+          <h2>{this.state.res}</h2>
+        </div>
+    );
+  }
 }
 
 export default App;
